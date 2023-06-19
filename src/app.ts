b29add9f079a0bccf9b6d2e46e25d8eb45eaedbd
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-19 17:47:49 -0300

    chore: add prisma intial config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
+const prisma = new PrismaClient();
 const app = express();
 
-export { app };
+export { app, prisma };
