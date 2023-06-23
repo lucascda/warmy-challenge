c5f4f9c2e2c6e8a95deb12c369f2c7d5ef3c3990
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-23 16:33:56 -0300

    docs: add swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import type { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { router } from './routes';
 import { type ApiError } from './utils/errors/apiError';
+import { serve, setup } from 'swagger-ui-express';
+import swagger from '../swagger.json';
 dotenv.config();
 
 const app = express();
@@ -18,5 +20,9 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
     rawErrors: err.rawErrors,
   });
 });
+
+// swagger
+
+app.use('/api-docs', serve, setup(swagger));
 
 export { app };
